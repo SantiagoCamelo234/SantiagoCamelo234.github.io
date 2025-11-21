f90a6bb83eaba0bc8ebaf8ad09d75ababd72f35d
SantiagoCamelo234 <ilvarcamelo206@gmail.com>
2025-11-21 11:17:59 -0500

    source changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {glob} from 'vite-plugin-gh-pages'
-
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: "/portfolio-ilvar-camelo/",
+  plugins: [react()],
+  base: '/portfolio/',
 })
-
-
-
